02e5fc0b09981766982e58a06d8043c2c429aa14
SV <saint-wp@163.com>
2024-11-06 22:34:20 +0800

    commit

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from util.svlog import logs
 ver = "2024-11-06 22:09:41"
 ts = 1730902181
 if __name__ == '__main__':
-    logs.logger.info(f'主程序启动，V1.0.1 ver={ver}')
+    logs.logger.info(f'RSS总控主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
