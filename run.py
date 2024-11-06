374b980303bf4c44def1730fd002470b4a905137
saintvamp <saint-wp@163.com>
2024-11-06 18:26:33 +0800

    update

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2024-11-06 18:16:55"
-ts = 1730888215
+ver = "2024-11-06 18:26:32"
+ts = 1730888792
 if __name__ == '__main__':
     logs.logger.info(f'主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
