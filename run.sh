e8417b658b81953f5f23e6a4870dd7b8d2193ff5
SV <saint-wp@163.com>
2024-12-03 23:35:49 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 git pull
 cp /auto_rss/model_config/model-config.toml /auto_rss/config/.
-python3 run.py &
+python3 run_rss.py &
 sleep 30s
 while true; do
   if [ -f /auto_rss/upgrade ]; then
     git pull
     cp /auto_rss/model_config/model-config.toml /auto_rss/config/.
-    ps | grep "python3 run.py" | grep -v grep | awk '{print $1}' | xargs kill -9
-    python3 run.py &
+    ps | grep "python3 run_rss.py" | grep -v grep | awk '{print $1}' | xargs kill -9
+    python3 run_rss.py &
     rm /auto_rss/upgrade
   fi
   sleep 5m
