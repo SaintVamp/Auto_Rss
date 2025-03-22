7cf1ec26445f436fcad3d9341c9bf3858bf35bc1
saintvamp <saint-wp@163.com>
2025-03-22 21:35:22 +0800

    update

--- run_rss.py
+++ run_rss.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-03-11 11:31:48"
-ts = 1741663908
+ver = "2025-03-22 21:35:22"
+ts = 1742650522
 if __name__ == '__main__':
     logs.logger.info(f'RSS总控主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
