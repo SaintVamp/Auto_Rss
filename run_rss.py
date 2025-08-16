ddc64eae87bc83643c6b220c936c9387f1e0ec17
SV <saint-wp@163.com>
2025-08-16 22:36:05 +0800

    commit

--- run_rss.py
+++ run_rss.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-07-27 09:10:44"
-ts = 1753578644
+ver = "2025-08-16 22:16:20"
+ts = 1755353780
 if __name__ == '__main__':
     logs.logger.info(f'RSS总控主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
