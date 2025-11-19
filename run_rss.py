bd949ac13a19f33243a44aec47b1a76bde4c0f59
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-11-19 03:26:11 +0000

    auto package

--- run_rss.py
+++ run_rss.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-11-18 06:35:31"
-ts = 1763447731
+ver = "2025-11-19 03:26:10"
+ts = 1763522770
 if __name__ == '__main__':
     logs.logger.info(f'RSS总控主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
